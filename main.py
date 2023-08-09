d30b7212b7785a19452ff3f9b5b95e56f5ad1c41
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-08-08 23:35:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ cur = conn.cursor()
 # get the names of the tables in the database
 cur.execute(" select name from sqlite_master where type = 'table' ") 
 
+# sql command
 SQL = """
 SELECT datetime(message.date/1000000000 + strftime("%s", "2001-01-01") ,"unixepoch","localtime") AS date_uct, id, text, is_from_me
 FROM message
